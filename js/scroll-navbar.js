6eeb3256fa98c243de33326fcf34dc367eb82dd3
Nathan Xiao <XDN2369@hotmail.com>
2020-02-14 20:23:17 +1100

    Add navbar scroll colour and scrollspy

---
+++ js/scroll-navbar.js
@@ -0,0 +1,11 @@
+const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+const navbar = document.getElementById("navbar");
+window.onscroll = () => {
+    if (this.scrollY < vh - 200) {
+        navbar.classList.remove("scrolled");
+    }
+    else {
+        navbar.classList.add("scrolled");
+    }
+
+};
