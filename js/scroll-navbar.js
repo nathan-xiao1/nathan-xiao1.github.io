e4902d31ee5203916739a5ad28eb9e296959cbb5
Nathan Xiao <XDN2369@hotmail.com>
2020-02-20 23:34:46 +1100

    Add navbar colour change on page loaded

--- js/scroll-navbar.js
+++ js/scroll-navbar.js
@@ -1,5 +1,6 @@
 const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 const navbar = document.getElementById("navbar");
+
 window.onscroll = () => {
     if (this.scrollY < vh - 200) {
         navbar.classList.remove("scrolled");
@@ -7,5 +8,8 @@ window.onscroll = () => {
     else {
         navbar.classList.add("scrolled");
     }
-
 };
+
+window.onload = () => {
+    if (window.scrollY) navbar.classList.add("scrolled");
+}
