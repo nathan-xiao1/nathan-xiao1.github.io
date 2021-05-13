32d2eb55cb01c01724691ec0b133b9b4da1a624a
Nathan Xiao <XDN2369@hotmail.com>
2021-05-13 11:00:27 +1000

    Update course information

--- js/courses.js
+++ js/courses.js
@@ -1,7 +1,5 @@
 var current = [
-    "COMP3121 Algorithms and Programming Techniques",
-    "COMP3141 Software System Design and Implementation",
-    "COMP6771 Advanced C++ Programming"
+    
 ]
 
 var completed = [
@@ -12,14 +10,20 @@ var completed = [
     "COMP2111 System Modelling and Design",
     "COMP2511 O-O Design & Programming",
     "COMP2521 Data Structures and Algorithms",
+    "COMP3121 Algorithms and Programming Techniques",
+    "COMP3141 Software System Design and Implementation",
     "COMP3231 Operating Systems",
     "COMP3311 Database Systems",
+    "COMP3331 Computer Networks and Applications",
     "COMP3411 Artificial Intelligence",
-    "COMP6441 Security Engineering"
+    "COMP6441 Security Engineering",
+    "COMP6771 Advanced C++ Programming",
+    "COMP9417 Machine Learning and Data Mining",
+    "COMP9444 Neural Networks and Deep Learning"
 ]
 
 var future = [
-    "COMP3331 Computer Networks and Applications"
+    
 ]
 
 function createDDTag(item) {
