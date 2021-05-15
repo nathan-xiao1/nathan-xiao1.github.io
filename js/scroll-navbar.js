5102cd24af07d2465063b7d435b80e51e6cd8788
Nathan Xiao <XDN2369@hotmail.com>
2021-05-15 12:06:05 +1000

    Fix event listener being overridden

--- js/scroll-navbar.js
+++ js/scroll-navbar.js
@@ -1,20 +1,26 @@
-const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+const vh = Math.max(
+  document.documentElement.clientHeight,
+  window.innerHeight || 0
+);
 const navbar = document.getElementById("navbar");
 
-window.onload = () => {
+window.addEventListener(
+  "load",
+  () => {
+    console.log(this.scrollY);
     if (this.scrollY < vh - 200) {
-        navbar.classList.remove("scrolled");
+      navbar.classList.remove("scrolled");
+    } else {
+      navbar.classList.add("scrolled");
     }
-    else {
-        navbar.classList.add("scrolled");
-    }
-}
+  },
+  false
+);
 
 window.onscroll = () => {
-    if (this.scrollY < vh - 200) {
-        navbar.classList.remove("scrolled");
-    }
-    else {
-        navbar.classList.add("scrolled");
-    }
+  if (this.scrollY < vh - 200) {
+    navbar.classList.remove("scrolled");
+  } else {
+    navbar.classList.add("scrolled");
+  }
 };
