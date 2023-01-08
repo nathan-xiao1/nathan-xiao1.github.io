3251cb569ef6d2f47f88697c0e9388c3a7c3984c
nathan-xiao1 <XDN2369@hotmail.com>
2023-01-08 11:46:26 +1100

    Add @styles alias

--- craco.config.ts
+++ craco.config.ts
@@ -3,7 +3,9 @@ import path from 'path';
 export default {
   webpack: {
     alias: {
+      '@app': path.resolve(__dirname, 'src/app'),
       '@assets': path.resolve(__dirname, 'src/assets'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
     },
   },
 };
--- src/app/home/codeblock/Codeblock.tsx
+++ src/app/home/codeblock/Codeblock.tsx
@@ -1,6 +1,5 @@
 import hljs from 'highlight.js/lib/core';
 import javascript from 'highlight.js/lib/languages/javascript';
-import json from 'highlight.js/lib/languages/json';
 import React, { useEffect } from 'react';
 import stripIndent from 'strip-indent';
 
@@ -9,7 +8,6 @@ import './hljs.scss';
 
 export function CodeBlock({ children }: { children: string }): JSX.Element {
   useEffect(() => {
-    hljs.registerLanguage('json', json);
     hljs.registerLanguage('javascript', javascript);
     hljs.highlightAll();
   }, []);
@@ -17,7 +15,7 @@ export function CodeBlock({ children }: { children: string }): JSX.Element {
   return (
     <div className="codeblock-container">
       <pre>
-        <code className="language-javascript text-small">
+        <code className="language-javascript">
           {stripIndent(children).trim()}
         </code>
       </pre>
